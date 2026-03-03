c1b07a6da217168fb4288e98545c0f18fe0742de
a10v <50676884+a10v@users.noreply.github.com>
2026-03-03 00:54:10 -0500

    fix: hardware req

--- DiscreteDiffusionVLA/sbatch/smoke/eval_libero_slurm_smoke_2a100_discrete_diffusion.sh
+++ DiscreteDiffusionVLA/sbatch/smoke/eval_libero_slurm_smoke_2a100_discrete_diffusion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=p32222
 #SBATCH --partition=gengpu
-#SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
-#SBATCH --mem=120G
+#SBATCH --mem=60G
 #SBATCH --time=47:00:00
 #SBATCH --job-name=openvla-eval-smoke-dd
 #SBATCH --output=logs/openvla_eval_smoke_dd_%j.out
