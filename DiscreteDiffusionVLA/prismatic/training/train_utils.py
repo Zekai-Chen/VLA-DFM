046d06952beaa2f00ce1be14ca9f70f3fd9bbcb2
a10v <50676884+a10v@users.noreply.github.com>
2026-03-03 15:09:29 -0500

    Align eval logic with checkpoints

--- DiscreteDiffusionVLA/prismatic/training/train_utils.py
+++ DiscreteDiffusionVLA/prismatic/training/train_utils.py
@@ -2,17 +2,32 @@
 
 import torch
 
-from prismatic.vla.constants import ACTION_DIM
+from prismatic.vla.constants import ACTION_DIM, ACTION_TOKEN_BEGIN_IDX, IGNORE_INDEX
 
 
-def get_current_action_mask(token_ids, action_begin: int, action_end: int):
+def _legacy_action_mask(token_ids, is_next: bool):
+    newline_positions = token_ids != IGNORE_INDEX
+    cumsum = torch.cumsum(newline_positions, dim=1)
+    if is_next:
+        mask = cumsum > ACTION_DIM
+    else:
+        mask = (1 <= cumsum) & (cumsum <= ACTION_DIM)
+    action_tokens_only_mask = token_ids > ACTION_TOKEN_BEGIN_IDX
+    return action_tokens_only_mask & mask
+
+
+def get_current_action_mask(token_ids, action_begin: int = None, action_end: int = None):
+    if action_begin is None or action_end is None:
+        return _legacy_action_mask(token_ids, is_next=False)
     is_action = (token_ids >= action_begin) & (token_ids < action_end)
     action_cumsum = torch.cumsum(is_action, dim=1)
     mask = (1 <= action_cumsum) & (action_cumsum <= ACTION_DIM)
     return is_action & mask
 
 
-def get_next_actions_mask(token_ids, action_begin: int, action_end: int):
+def get_next_actions_mask(token_ids, action_begin: int = None, action_end: int = None):
+    if action_begin is None or action_end is None:
+        return _legacy_action_mask(token_ids, is_next=True)
     is_action = (token_ids >= action_begin) & (token_ids < action_end)
     action_cumsum = torch.cumsum(is_action, dim=1)
     mask = action_cumsum > ACTION_DIM
